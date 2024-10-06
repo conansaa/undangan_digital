c8b396c769f410e9afaa7ca7ce51b3f29ca0a95a
Kamilafp <kamilafp25@gmail.com>
2024-10-06 15:36:46 +0700

    o

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -6,11 +6,16 @@
     <title>Daftar Komentar</title>
 </head>
 <body>
-    <h1>Daftar Ucapan untuk Mempelai</h1>
+    <h1>Daftar Konfirmasi Kehadiran Tamu</h1>
 
     <!-- Tampilkan pesan sukses jika ada -->
     @if (session('success'))
         <p style="color: green;">{{ session('success') }}</p>
+    @endif
+
+    <!-- Tampilkan URL spreadsheet jika ada -->
+    @if (session('spreadsheetUrl'))
+        <p>Link ke Google Sheets: <a href="{{ session('spreadsheetUrl') }}" target="_blank">{{ session('spreadsheetUrl') }}</a></p>
     @endif
 
     <!-- Form untuk menambahkan komentar -->
@@ -18,11 +23,13 @@
         @csrf
         <input type="text" name="name" placeholder="Nama" required><br>
         <textarea name="message" placeholder="Pesan" required></textarea><br>
+        <input type="checkbox" name="submit_to_google_sheets" id="submit_to_google_sheets" value="yes">
+        <label for="submit_to_google_sheets">Submit data to Google Sheets</label><br>   
         <button type="submit">Kirim</button>
     </form>
 
     <!-- Daftar komentar -->
-    <h2>Ucapan dari Tamu</h2>
+    <h2>Kehadiran Tamu</h2>
     <ul>
         @foreach ($comments as $comment)
             <li>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\GoogleSheetsController;
 
 Route::get('/', [CommentController::class, 'index']);
 
