d2c9281eaa545ac5b49b64d59f3685253e7b6bea
Kamilafp <kamilafp25@gmail.com>
2025-01-04 18:35:34 +0700

    RSVP Client
    agar saat login maka data rsvp dan komen yang ditampilkan sesuai event id yang dimiliki user tersebut

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,20 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Rsvp;
 use App\Models\Comments;
-use Illuminate\Http\Request;
+use App\Models\EventOwnerNew;
+use Illuminate\Support\Facades\Auth;
 
 class ClientController extends Controller
 {
     public function showDashboard()
     {
-        // Calculate total guests where confirmation is 'Hadir'
-        $totalGuests = Rsvp::where('confirmation', 'Hadir')->sum('total_guest');
-        // Calculate total comments
+        $userId = Auth::id();
+
+        $eventOwner = EventOwnerNew::where('user_id', $userId)->first();
+
+        if (!$eventOwner) {
+            return redirect()->route('home')->withErrors('Event Owner tidak ditemukan.');
+        }
+
+        $eventDetailsIds = $eventOwner->event()->pluck('id');
+
+        $totalGuests = Rsvp::whereIn('event_id', $eventDetailsIds)
+            ->where('confirmation', 'Hadir')
+            ->sum('total_guest');
+
         $totalComments = Comments::count();
 
         return view('client.client', compact('totalGuests', 'totalComments'));
-
     }
-
 }
 
